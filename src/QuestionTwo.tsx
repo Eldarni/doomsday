543d5f756eb5f9a1a8106b98b512b1cf393d0fd8
Eldarni <hello@eldarni.me>
2026-04-30 22:34:56 +0100

    tidy up the question 2 helptext

--- src/QuestionTwo.tsx
+++ src/QuestionTwo.tsx
@@ -89,16 +89,16 @@ function QuestionTwo(props: { cheatMode: boolean, targetDate: Date }) {
                 }
 
                 {(showCorrectAnswer === true || showHelpText === true) && 
-                    <div className="helpText">
+                    <div className="helpText helpTextQuestionTwo">
       
-                        <div className="helpTextContent">
+                        <main>
                             <p>To calculate the anchor day for any year, follow these steps:</p>
                             <ul>
 
-                                <li>Take the last 2 digits of the year so <code>{year} == {T0}</code>.</li>
+                                <li>Take the last 2 digits of the year so <code>{year} = {T0}</code>.</li>
                                 
                                 { T0 % 2 === 1 ? (
-                                    <li>As {T0} is odd, add 11, so <code>{T0} + 11 == {T1}</code>.</li>
+                                    <li>As {T0} is odd, add 11, so <code>{T0} + 11 = {T1}</code>.</li>
                                 ) : (
                                     <li>As {T0} is even, do nothing, (if it was odd, we would have added 11).</li>
                                 )}
@@ -106,7 +106,7 @@ function QuestionTwo(props: { cheatMode: boolean, targetDate: Date }) {
                                 <li>Divide {T1} by 2, so <code>{T1} / 2 = {T2}</code>.</li>
 
                                 { T2 % 2 === 1 ? (
-                                    <li>As {T2} is odd, add 11, so <code>{T2} + 11 == {T3}</code>.</li>
+                                    <li>As {T2} is odd, add 11, so <code>{T2} + 11 = {T3}</code>.</li>
                                 ) : (
                                     <li>As {T2} is even, do nothing, (if it was odd, we would have added 11).</li>
                                 )}
@@ -114,28 +114,28 @@ function QuestionTwo(props: { cheatMode: boolean, targetDate: Date }) {
                                 { T3 % 7 === 0 ? (
                                     <li>As {T3} is divisible by 7, we can skip the next step.</li>    
                                 ) : (
-                                    <li>Work out the remainder when dividing by 7 <code>({T3} % 7 == {T3 % 7})</code>, as {T3} is slightly more than {Math.floor(T3 / 7) * 7} we can just do <code>{T3} - {Math.floor(T3 / 7) * 7} == {T4}</code>.</li>
+                                    <li>Work out the remainder when dividing by 7 <code>({T3} % 7 = {T3 % 7})</code>, as {T3} is slightly more than {Math.floor(T3 / 7) * 7} we can just do <code>{T3} - {Math.floor(T3 / 7) * 7} = {T4}</code>.</li>
                                 ) }
 
                                 <li>Subtract {T4} from 7, so <code>7 - {T4} == {T5}</code></li>
                                 
                                 <li>Then add the result to the anchor day for the century (which is {getNameOfDay(AC)} or {AC}) <code>{T5} + {AC} == {T6}</code>.</li>
 
-                                <li>Finally, take the result and wrap it around if it's greater than 6, so <code>{T6} % 7 == {T6 % 7}</code>.</li>
+                                <li>Finally, take the result and wrap it around if it's greater than 6, so <code>{T6} mod 7 = {T6 % 7}</code>.</li>
 
                                 <li><b>The answer is {getNameOfDay(T6 % 7)}!</b></li>
 
                             </ul>
-                        </div>
+                        </main>
 
-                        <div className="helpTextTable">
+                        <aside>
                             {Array.from({ length: 14 }).map((_, i) => {
                                 const multiple = (i + 1) * 7;
                                 return (
                                     <div key={i} className={T3 > multiple && T3 < multiple + 7 ? "highlight" : ""}>7 × {i + 1} = {multiple}</div>
                                 )
                             })}
-                        </div>
+                        </aside>
 
                     </div>
                 }
